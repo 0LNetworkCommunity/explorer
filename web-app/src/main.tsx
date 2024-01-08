370ae162971f6abd380aa84880ef2b742f742484
Minaxolone <minaxolone@gmail.com>
2024-01-08 17:45:47 +0000

    posthog added

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './modules/core/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./modules/core/App";
+import { PostHogProvider } from "posthog-js/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </React.StrictMode>
 );

3c2257aa92fa3d4b54791fd2c5ac029b6537101f
Minaxolone <minaxolone@gmail.com>
2024-01-21 20:36:55 +0000

    build script added

---
+++ api/build.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+npm run build
+
+CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
+
+docker build \
+  --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
+  --file ./Dockerfile \
+  --tag "registry.gitlab.com/0lfyi/explorer/api:$CI_COMMIT_SHA" \
+  .

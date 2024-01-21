3d6ae2d810925d46e5cb28f63a5098b8d60e9339
Minaxolone <minaxolone@gmail.com>
2024-01-21 19:00:06 +0000

    wip

--- web-app/build.sh
+++ web-app/build.sh
@@ -7,5 +7,5 @@ CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
 docker build \
   --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
   --file ./docker/Dockerfile \
-  --tag "registry.gitlab.com/0lfyi/explorer/web-app:7de5962f" \
+  --tag "registry.gitlab.com/0lfyi/explorer/web-app:$CI_COMMIT_SHA" \
   .

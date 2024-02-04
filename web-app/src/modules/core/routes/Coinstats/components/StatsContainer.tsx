bf3946a9b1cc34f9dc0cb169da77ae4792df710f
Caius Camillus <157544279+caius-camillus@users.noreply.github.com>
2024-02-04 18:30:12 +0000

    fix responsive dividers

--- web-app/src/modules/core/routes/Coinstats/components/StatsContainer.tsx
+++ web-app/src/modules/core/routes/Coinstats/components/StatsContainer.tsx
@@ -12,8 +12,8 @@ const StatsContainer: FC<Props> = ({ columns, children }) => {
         "mt-2",
         "rounded-lg bg-white shadow overflow-hidden",
         "mx-auto grid bg-gray-900/5",
-        `lg:grid-cols-${columns} lg:divide-x`,
-        `sm:grid-cols-1 sm:divide-y`
+        `lg:grid-cols-${columns} lg:divide-x lg:divide-y-0`,
+        `grid-cols-1 divide-y`
       )}
     >
       {children}

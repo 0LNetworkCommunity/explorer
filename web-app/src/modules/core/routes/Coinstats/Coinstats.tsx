dfa1b883fc78124495c314f9367af6b90da1582b
Caius Camillus <157544279+caius-camillus@users.noreply.github.com>
2024-02-04 17:51:56 +0000

    coinstats loading state added

--- web-app/src/modules/core/routes/Coinstats/Coinstats.tsx
+++ web-app/src/modules/core/routes/Coinstats/Coinstats.tsx
@@ -11,10 +11,21 @@ const getData = async () => {
 };
 
 const Coinstats = () => {
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState<any>();
 
   useEffect(() => {
-    getData().then(setData);
+    const load = async () => {
+      try {
+        setLoading(true);
+        setData(await getData());
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    load();
   }, []);
 
   if (!data) {
@@ -23,152 +34,158 @@ const Coinstats = () => {
 
   return (
     <div className="p-4">
-      <div>
-        <h3 className="text-base font-semibold text-gray-900">
-          Supply
-        </h3>
+      {(!data && loading) && <div>loading...</div>}
 
-        <StatsContainer columns={2}>
-          <StatItem
-            name="Circulating supply"
-            secondary={`${data.circulatingSupply.percentage.toFixed(4)}%`}
-          >
-            <Money>{data.circulatingSupply.nominal}</Money>
-          </StatItem>
+      {data && (
+        <>
+          <div>
+            <h3 className="text-base font-semibold text-gray-900">Supply</h3>
 
-          <StatItem
-            name="Total burned since Genesis"
-            secondary={`${data.totalBurned.percentage.toFixed(4)}%`}
-          >
-            <Money>{data.totalBurned.nominal}</Money>
-          </StatItem>
-        </StatsContainer>
+            <StatsContainer columns={2}>
+              <StatItem
+                name="Circulating supply"
+                secondary={`${data.circulatingSupply.percentage.toFixed(4)}%`}
+              >
+                <Money>{data.circulatingSupply.nominal}</Money>
+              </StatItem>
 
-        <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-3">
-          <ChartComponent
-            type="PieChart"
-            title="Individuals capital"
-            data={data.individualsCapital}
-          />
+              <StatItem
+                name="Total burned since Genesis"
+                secondary={`${data.totalBurned.percentage.toFixed(4)}%`}
+              >
+                <Money>{data.totalBurned.nominal}</Money>
+              </StatItem>
+            </StatsContainer>
 
-          <ChartComponent
-            type="PieChart"
-            title="Total Supply"
-            data={data.supplyAllocation}
-          />
+            <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-3">
+              <ChartComponent
+                type="PieChart"
+                title="Individuals capital"
+                data={data.individualsCapital}
+              />
 
-          <ChartComponent
-            type="PieChart"
-            title="Community capital"
-            data={data.communityCapital}
-          />
-        </dl>
+              <ChartComponent
+                type="PieChart"
+                title="Total Supply"
+                data={data.supplyAllocation}
+              />
 
-        <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-2">
-          <ChartComponent
-            type="BarChart"
-            title="Accounts on chain over time"
-            data={data.accountsOnChainOverTime}
-          />
-          <ChartComponent
-            type="LineAndAreaChart"
-            title="Total Burned Over Time"
-            data={data.burnOverTime}
-          />
-        </dl>
-      </div>
+              <ChartComponent
+                type="PieChart"
+                title="Community capital"
+                data={data.communityCapital}
+              />
+            </dl>
 
-      <div className="mt-10">
-        <h3 className="text-base font-semibold text-gray-900">
-          Validators
-        </h3>
+            <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-2">
+              <ChartComponent
+                type="BarChart"
+                title="Accounts on chain over time"
+                data={data.accountsOnChainOverTime}
+              />
+              <ChartComponent
+                type="LineAndAreaChart"
+                title="Total Burned Over Time"
+                data={data.burnOverTime}
+              />
+            </dl>
+          </div>
 
-        <StatsContainer columns={2}>
-          <StatItem
-            name="Epoch reward"
-            secondary={`${data.lastEpochReward.percentage.toFixed(4)}%`}
-          >
-            <Money>{data.lastEpochReward.nominal}</Money>
-          </StatItem>
+          <div className="mt-10">
+            <h3 className="text-base font-semibold text-gray-900">
+              Validators
+            </h3>
 
-          <StatItem name="Clearing Bid">
-            {`${data.currentClearingBid}%`}
-          </StatItem>
-        </StatsContainer>
+            <StatsContainer columns={2}>
+              <StatItem
+                name="Epoch reward"
+                secondary={`${data.lastEpochReward.percentage.toFixed(4)}%`}
+              >
+                <Money>{data.lastEpochReward.nominal}</Money>
+              </StatItem>
 
-        <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-2">
-          <ChartComponent
-            type="LineChart"
-            title="Rewards Over Time"
-            data={data.rewardsOverTime}
-          />
-          <ChartComponent
-            type="LineChart"
-            title="Clearing bid Over Time"
-            data={data.clearingBidoverTime}
-          />
-        </dl>
-      </div>
+              <StatItem name="Clearing Bid">
+                {`${data.currentClearingBid}%`}
+              </StatItem>
+            </StatsContainer>
 
-      <div className="mt-10">
-        <h3 className="text-base font-semibold text-gray-900">
-          Community Wallets
-        </h3>
+            <dl className="mt-5 grid grid-cols-1 gap-5 lg:grid-cols-2">
+              <ChartComponent
+                type="LineChart"
+                title="Rewards Over Time"
+                data={data.rewardsOverTime}
+              />
+              <ChartComponent
+                type="LineChart"
+                title="Clearing bid Over Time"
+                data={data.clearingBidoverTime}
+              />
+            </dl>
+          </div>
 
-        <StatsContainer columns={1}>
-          <StatItem
-            name="Community Wallets Balance"
-            secondary={`${data.communityWalletsBalance.percentage.toFixed(4)}%`}
-          >
-            <Money>{data.communityWalletsBalance.nominal}</Money>
-          </StatItem>
-        </StatsContainer>
+          <div className="mt-10">
+            <h3 className="text-base font-semibold text-gray-900">
+              Community Wallets
+            </h3>
 
-        <dl className="mt-5 grid grid-cols-1 gap-5">
-          <ChartComponent
-            type="PieChart"
-            title="Community wallets breakdown"
-            data={data.communityWalletsBalanceBreakdown}
-          />
-        </dl>
-      </div>
+            <StatsContainer columns={1}>
+              <StatItem
+                name="Community Wallets Balance"
+                secondary={`${data.communityWalletsBalance.percentage.toFixed(
+                  4
+                )}%`}
+              >
+                <Money>{data.communityWalletsBalance.nominal}</Money>
+              </StatItem>
+            </StatsContainer>
 
-      <div className="mt-5">
-        <h3 className="text-base font-semibold text-gray-900">
-          Slow Wallets
-        </h3>
+            <dl className="mt-5 grid grid-cols-1 gap-5">
+              <ChartComponent
+                type="PieChart"
+                title="Community wallets breakdown"
+                data={data.communityWalletsBalanceBreakdown}
+              />
+            </dl>
+          </div>
 
-        <StatsContainer columns={3}>
-          <StatItem name="Slow wallets count">
-            {`${data.currentSlowWalletsCount.toLocaleString()}`}
-          </StatItem>
-          <StatItem
-            name="Locked on Slow wallets"
-            secondary={`${data.currentLockedOnSlowWallets.percentage.toFixed(
-              4
-            )}%`}
-          >
-            <Money>{data.currentLockedOnSlowWallets.nominal}</Money>
-          </StatItem>
+          <div className="mt-5">
+            <h3 className="text-base font-semibold text-gray-900">
+              Slow Wallets
+            </h3>
 
-          <StatItem
-            name="Last Epoch Unlocked"
-            secondary={`${data.lastEpochTotalUnlockedAmount.percentage.toFixed(
-              4
-            )}%`}
-          >
-            <Money>{data.lastEpochTotalUnlockedAmount.nominal}</Money>
-          </StatItem>
-        </StatsContainer>
+            <StatsContainer columns={3}>
+              <StatItem name="Slow wallets count">
+                {`${data.currentSlowWalletsCount.toLocaleString()}`}
+              </StatItem>
+              <StatItem
+                name="Locked on Slow wallets"
+                secondary={`${data.currentLockedOnSlowWallets.percentage.toFixed(
+                  4
+                )}%`}
+              >
+                <Money>{data.currentLockedOnSlowWallets.nominal}</Money>
+              </StatItem>
 
-        <dl className="mt-5 grid grid-cols-1 gap-5">
-          <ChartComponent
-            type="LineAndBarChart"
-            title="Slow wallets count Over Time"
-            data={data.slowWalletsCountOverTime}
-          />
-        </dl>
-      </div>
+              <StatItem
+                name="Last Epoch Unlocked"
+                secondary={`${data.lastEpochTotalUnlockedAmount.percentage.toFixed(
+                  4
+                )}%`}
+              >
+                <Money>{data.lastEpochTotalUnlockedAmount.nominal}</Money>
+              </StatItem>
+            </StatsContainer>
+
+            <dl className="mt-5 grid grid-cols-1 gap-5">
+              <ChartComponent
+                type="LineAndBarChart"
+                title="Slow wallets count Over Time"
+                data={data.slowWalletsCountOverTime}
+              />
+            </dl>
+          </div>
+        </>
+      )}
     </div>
   );
 };

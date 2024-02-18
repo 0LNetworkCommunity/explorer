e3ed1352efac1d5d13820725a34a414fbcc7fa91
Leirbag <gabrielelfassi@yahoo.fr>
2024-02-18 18:32:34 +0100

    feat(home): table responsive style

--- web-app/src/modules/core/routes/Home/Home.tsx
+++ web-app/src/modules/core/routes/Home/Home.tsx
@@ -1,10 +1,10 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
-import { gql, useQuery } from "@apollo/client";
-import clsx from "clsx";
-import Page from "../../../ui/Page/Page";
-import Stats from "./Stats";
-import TransactionsTable from "../../../ui/UserTransactionsTable";
+import { gql, useQuery } from '@apollo/client';
+import clsx from 'clsx';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import Page from '../../../ui/Page/Page';
+import TransactionsTable from '../../../ui/UserTransactionsTable';
+import Stats from './Stats';
 
 const GET_USER_TRANSACTIONS = gql`
   query GetUserTransactions {
@@ -26,9 +26,7 @@ const Transactions: FC = () => {
   const { data } = useQuery(GET_USER_TRANSACTIONS);
 
   if (data) {
-    return (
-      <TransactionsTable transactions={data.userTransactions.items} />
-    );
+    return <TransactionsTable transactions={data.userTransactions.items} />;
   }
 
   return null;
@@ -38,26 +36,25 @@ const Home: FC = () => {
   return (
     <Page>
       <Stats />
-      <section className="mt-2">
+      <section className="mt-2 overflow-x-auto">
         <Transactions />
-
-        <div className="flex p-4 items-center justify-center">
-          <Link
-            to="/transactions"
-            className={clsx(
-              "rounded-md bg-primary-500",
-              "px-3.5 py-2.5",
-              "text-sm font-semibold text-white",
-              "shadow-sm",
-              "hover:bg-primary-600",
-              "focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
-              "focus-visible:outline-primary-600"
-            )}
-          >
-            See all transactions
-          </Link>
-        </div>
       </section>
+      <div className="flex p-4 items-center justify-center">
+        <Link
+          to="/transactions"
+          className={clsx(
+            'rounded-md bg-primary-500',
+            'px-3.5 py-2.5',
+            'text-sm font-semibold text-white',
+            'shadow-sm',
+            'hover:bg-primary-600',
+            'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+            'focus-visible:outline-primary-600',
+          )}
+        >
+          See all transactions
+        </Link>
+      </div>
     </Page>
   );
 };

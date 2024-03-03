12d7cc468eefdf081173d14ffeee191d1974124b
minaxolone <minaxolone@gmail.com>
2024-03-03 17:22:49 +0000

    fix posthog integration

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,30 +1,39 @@
 // import React from "react";
 
+import { PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
 
 import App from './modules/core/App';
 
 import './index.css';
 
 const { VITE_POSTHOG_KEY, VITE_POSTHOG_HOST } = import.meta.env;
-if (VITE_POSTHOG_KEY && VITE_POSTHOG_HOST) {
-  const posthog = require('posthog-js');
-  const { PostHogProvider } = require('posthog-js/react');
-  posthog.init(VITE_POSTHOG_KEY, {
-    api_host: VITE_POSTHOG_HOST,
-  });
 
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    // <React.StrictMode>
-    <PostHogProvider client={posthog}>
-      <App />
-    </PostHogProvider>,
-    // </React.StrictMode>
-  );
-} else {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    // <React.StrictMode>
-    <App />,
-    // </React.StrictMode>
-  );
-}
+const PostHogProviderWrapper = ({ children }: PropsWithChildren) => {
+  if (VITE_POSTHOG_KEY && VITE_POSTHOG_HOST) {
+    posthog.init(VITE_POSTHOG_KEY, {
+      api_host: VITE_POSTHOG_HOST,
+    });
+
+    return (
+      <PostHogProvider
+        apiKey={VITE_POSTHOG_KEY}
+        options={{ api_host: VITE_POSTHOG_HOST }}
+        client={posthog}
+      >
+        {children}
+      </PostHogProvider>
+    );
+  }
+  return <>{children}</>;
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // <React.StrictMode>
+  <PostHogProviderWrapper>
+    <App />
+  </PostHogProviderWrapper>,
+  // </React.StrictMode>
+);

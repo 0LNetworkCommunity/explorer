c654a931d02885c353a1c17c7a0256531f64ebda
minaxolone <minaxolone@gmail.com>
2024-03-14 17:28:20 +0000

    add missing table queries

---
+++ api/src/clickhouse/queries/boundary_status.sql
@@ -0,0 +1,30 @@
+INSERT INTO "boundary_status" (
+    "version",
+    "timestamp",
+    "change_index",
+    "incoming_fees",
+    "outgoing_nominal_reward_to_vals",
+    "outgoing_total_reward",
+    "system_fees_collected"
+)
+SELECT
+    "version",
+    "timestamp",
+    "change_index",
+    "incoming_fees",
+    "outgoing_nominal_reward_to_vals",
+    "outgoing_total_reward",
+    "system_fees_collected"
+FROM
+  input(
+    '
+      version UInt64,
+      timestamp UInt64,
+      change_index UInt64,
+      incoming_fees UInt64,
+      outgoing_nominal_reward_to_vals UInt64,
+      outgoing_total_reward UInt64,
+      system_fees_collected UInt64
+    '
+  )
+  FORMAT Parquet

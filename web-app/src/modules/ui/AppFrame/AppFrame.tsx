21add2b8149d2aaf81f82a2c53996cd360241362
minaxolone <minaxolone@gmail.com>
2024-03-16 11:03:33 +0000

    feat: add maintenance banner

--- web-app/src/modules/ui/AppFrame/AppFrame.tsx
+++ web-app/src/modules/ui/AppFrame/AppFrame.tsx
@@ -1,10 +1,13 @@
 import { FC, PropsWithChildren } from 'react';
 
 import Header from '../Layout/Header';
+import Banner from '../Banner';
 
 const AppFrame: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className='min-h-full flex flex-col'>
+      <Banner />
+
       <Header />
       {children}
     </div>

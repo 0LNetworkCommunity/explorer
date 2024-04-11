cccb040e14a60caf0ebb2d71d1c8cce5008be90b
minaxolone <minaxolone@gmail.com>
2024-04-10 22:02:07 -0400

    hide maintenance banner

--- web-app/src/modules/ui/AppFrame/AppFrame.tsx
+++ web-app/src/modules/ui/AppFrame/AppFrame.tsx
@@ -1,12 +1,12 @@
 import { FC, PropsWithChildren } from 'react';
 
 import Header from '../Layout/Header';
-import Banner from '../Banner';
+// import Banner from '../Banner';
 
 const AppFrame: FC<PropsWithChildren> = ({ children }) => {
   return (
     <div className='min-h-full flex flex-col'>
-      <Banner />
+      {/* <Banner /> */}
 
       <Header />
       {children}

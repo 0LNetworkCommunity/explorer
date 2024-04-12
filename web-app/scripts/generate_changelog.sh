9715400466498dbf554dad10abb2940f2bc9f9b1
Leirbag <gabrielelfassi@yahoo.fr>
2024-04-12 17:26:10 +0200

    new upload changelog script

--- web-app/scripts/generate_changelog.sh
+++ web-app/scripts/generate_changelog.sh
@@ -11,26 +11,17 @@ ADDED_ENTRIES=""
 CHANGED_ENTRIES=""
 REMOVED_ENTRIES=""
 
-VERSION_PATTERN='^\[([0-9]+)\.([0-9]+)\.([0-9]+)\]'
-
-VERSIONS=()
-while IFS= read -r line; do
-    if [[ $line =~ $VERSION_PATTERN ]]; then
-        VERSIONS+=("${BASH_REMATCH[1]}.${BASH_REMATCH[2]}.${BASH_REMATCH[3]}")
-    fi
-done < <(git log --pretty=format:"%s" origin/feat/lei_changelog...origin/feat/lei_changelog_dev)
-
-
-LATEST_VERSION="0.0.0"
-for version in "${VERSIONS[@]}"; do
-    if [[ "$version" > "$LATEST_VERSION" ]]; then
-        LATEST_VERSION=$version
-    fi
-done
+# Use jq to read the version number directly from package.json
+if [ -f "package.json" ]; then
+    LATEST_VERSION=$(jq -r '.version' package.json)
+else
+    echo "package.json not found. Exiting..."
+    exit 1
+fi
 
 CURRENT_DATE=$(date "+%Y-%m-%d")
 
-
+# Process git log entries and categorize them into Added, Changed, and Removed
 while IFS= read -r commit; do
     case "$commit" in
     "[+]"*) ADDED_ENTRIES+="- ${commit:4}\n" ;;
@@ -39,9 +30,9 @@ while IFS= read -r commit; do
     esac
 done < <(git log --pretty=format:"%s" origin/feat/lei_changelog...origin/feat/lei_changelog_dev)
 
-
 NEW_ENTRY="## [$LATEST_VERSION] - $CURRENT_DATE\n\n### Added\n\n$ADDED_ENTRIES\n### Changed\n\n$CHANGED_ENTRIES\n### Removed\n\n$REMOVED_ENTRIES\n"
 
+# Prepend new changelog entries to the existing CHANGELOG.md, or create it if it doesn't exist
 if [ -f "CHANGELOG.md" ]; then
     echo -e "$NEW_ENTRY$(cat CHANGELOG.md)" > TEMP_CHANGELOG.md
     mv TEMP_CHANGELOG.md CHANGELOG.md
@@ -53,5 +44,6 @@ git add CHANGELOG.md
 git commit -m "chore: update changelog"
 git push origin main
 
+# Tag the current commit with the latest version and push the tag to the remote repository
 git tag -a "$LATEST_VERSION" -m "Release $LATEST_VERSION"
 git push origin "$LATEST_VERSION"

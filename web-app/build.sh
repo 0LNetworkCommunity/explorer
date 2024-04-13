deed62081feca4fe136a8b52ceaea4072910c0b8
minaxolone <minaxolone@gmail.com>
2024-04-13 19:29:31 +0000

    build script added

--- web-app/build.sh
+++ web-app/build.sh
@@ -9,5 +9,5 @@ CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
 docker build \
   --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
   --file ./docker/Dockerfile \
-  --tag "registry.gitlab.com/0lfyi/explorer/web-app:$CI_COMMIT_SHA" \
+  --tag "ghcr.io/0lnetworkcommunity/explorer/web-app:$CI_COMMIT_SHA" \
   .

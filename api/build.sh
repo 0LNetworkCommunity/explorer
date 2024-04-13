b72e64a20e8eb676fe3c688df62dd274cfd83ce4
minaxolone <minaxolone@gmail.com>
2024-04-13 19:21:00 +0000

    update docker image name

--- api/build.sh
+++ api/build.sh
@@ -8,5 +8,5 @@ CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
 docker build \
   --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
   --file ./Dockerfile \
-  --tag "registry.gitlab.com/0lfyi/explorer/api:$CI_COMMIT_SHA" \
+  --tag "ghcr.io/0lnetworkcommunity/explorer/api:$CI_COMMIT_SHA" \
   .

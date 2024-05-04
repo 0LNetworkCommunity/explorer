fa50ae86049d3273660d11f5f83a676f4b66dd67
minaxolone <minaxolone@gmail.com>
2024-05-04 17:57:51 -0400

    add circulating and total supply endpoints

--- api/src/stats/stats.controller.ts
+++ api/src/stats/stats.controller.ts
@@ -17,12 +17,24 @@ export class StatsController {
     this.cacheEnabled = config.get<boolean>('cacheEnabled')!;
   }
 
+  @Get("/total-supply")
+  public async getTotalSupply(@Res() res: Response) {
+    const totalSupply = await this.statsService.getTotalSupply();
+    res.send({ totalSupply });
+  }
+
+  @Get("/circulating-supply")
+  public async getCirculatingSupply(@Res() res: Response) {
+    const circulatingSupply = await this.statsService.getCirculatingSupply();
+    res.send({ circulatingSupply });
+  }
+
   @Get()
-  public async getStats(@Query('q') query: string, @Res() res: Response) {
+  public async getStats(@Res() res: Response) {
     res.set('Content-Type', 'application/json');
 
     // Check if caching is enabled and the query is not present
-    if (this.cacheEnabled && !query) {
+    if (this.cacheEnabled) {
       const cachedStats = await redisClient.get(STATS_CACHE_KEY);
       if (cachedStats) {
         res.send(cachedStats);
@@ -31,20 +43,7 @@ export class StatsController {
       throw new ServiceUnavailableException('Cache not ready');
     }
 
-    // Handle specific queries based on the 'q' parameter
-    switch (query) {
-      case 'circulatingSupply':
-        const circulatingSupply = await this.statsService.getCirculatingSupply();
-        res.send({ circulatingSupply });
-        break;
-      case 'totalSupply':
-        const totalSupply = await this.statsService.getTotalSupply();
-        res.send({ totalSupply });
-        break;
-      default:
-        // Handle the general stats case or no query parameter provided
-        const stats = await this.statsService.getStats();
-        res.send(stats);
-    }
+    const stats = await this.statsService.getStats();
+    res.send(stats);
   }
 }

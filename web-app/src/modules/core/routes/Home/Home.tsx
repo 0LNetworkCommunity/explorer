48ea5405aaa733e4e226a7b30a390f8d6db4d148
minaxolone <minaxolone@gmail.com>
2024-05-17 21:09:44 -0700

    fix build

--- web-app/src/modules/core/routes/Home/Home.tsx
+++ web-app/src/modules/core/routes/Home/Home.tsx
@@ -1,5 +1,4 @@
 import { gql, useQuery } from '@apollo/client';
-import clsx from 'clsx';
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import Page from '../../../ui/Page/Page';

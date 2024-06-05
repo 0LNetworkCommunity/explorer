dd70f57c6d26a36b3189cbfc69c1dc4ae553da21
saki-osive <saki.osive@gmail.com>
2024-06-05 08:13:10 +0530

    Add Bullboard Service

--- api/src/bullboard/bullboard.service.ts
+++ api/src/bullboard/bullboard.service.ts
@@ -3,14 +3,37 @@ import { Queue } from 'bullmq';
 import { createBullBoard } from '@bull-board/api';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter.js';
 import { ExpressAdapter } from '@bull-board/express';
+import { redisClient } from "../redis/redis.service.js";
 
 @Injectable()
 export class BullBoardService {
 
   private readonly serverAdapter: ExpressAdapter;
+  private readonly queueNames: string[] = [
+    "ol-clickhouse-ingestor",
+    "ol-parquet-producer",
+    "ol-version-batch",
+    "ol-version",
+    "expired-transactions",
+    "ol-swap",
+    "node-watcher",
+    "wallet-subscription",
+    "stats"
+  ];
 
   constructor() {
     this.serverAdapter = new ExpressAdapter();
+  }
+
+  onModuleInit() {
+    if (process.env.NODE_ENV !== 'production') {
+      this.setupBullBoard(
+        this.queueNames.map(name => ({
+          name,
+          connection: redisClient,
+        }))
+      );
+    }
   }
 
   setupBullBoard(queueConfigs: { name: string; connection: any }[]) {

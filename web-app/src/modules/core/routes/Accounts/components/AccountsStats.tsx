a778774762a7f8af2c829196b87d8cb001bc80c5
soaresa <10797037+soaresa@users.noreply.github.com>
2024-06-07 07:18:44 -0300

    fix infinite loop call of accounts stats

--- web-app/src/modules/core/routes/Accounts/components/AccountsStats.tsx
+++ web-app/src/modules/core/routes/Accounts/components/AccountsStats.tsx
@@ -16,31 +16,31 @@ const AccountsStats: FC = () => {
     const load = async () => {
       const res = await getData();
       setData(res);
-      console.log(data);
     };
+
     load();
-  }, [data]);
+  }, []); // The empty dependency array ensures this runs only once after the initial render
 
   return (
     <div className="grid grid-cols-2 gap-[4px] md:grid-cols-3 lg:grid-cols-4 pb-8">
       <StatsCard
         title="Total Accounts"
-        value={data?.totalAccounts.toLocaleString()}
+        value={data?.totalAccounts?.toLocaleString()}
         loading={!data}
       />
       <StatsCard
         title="24h Active Accounts"
-        value={data?.activeAddressesCount?.lastDay.toLocaleString()}
+        value={data?.activeAddressesCount?.lastDay?.toLocaleString()}
         loading={!data}
       />
       <StatsCard
         title="30d Active Accounts"
-        value={data?.activeAddressesCount?.last30Days.toLocaleString()}
+        value={data?.activeAddressesCount?.last30Days?.toLocaleString()}
         loading={!data}
       />
       <StatsCard
         title="90d Active Accounts"
-        value={data?.activeAddressesCount?.last90Days.toLocaleString()}
+        value={data?.activeAddressesCount?.last90Days?.toLocaleString()}
         loading={!data}
       />
     </div>

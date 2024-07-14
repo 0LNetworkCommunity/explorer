dfdc7c93f4a20ec130aac840112ce396dc710ac6
minaxolone <minaxolone@gmail.com>
2024-07-14 17:03:19 +0000

    update docker registry

--- api/build.sh
+++ api/build.sh
@@ -8,5 +8,5 @@ CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
 docker build \
   --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
   --file ./Dockerfile \
-  --tag "ghcr.io/0lnetworkcommunity/explorer/api:$CI_COMMIT_SHA" \
+  --tag "ghcr.io/minaxolone/explorer/api:$CI_COMMIT_SHA" \
   .

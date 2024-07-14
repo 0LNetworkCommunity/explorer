e548b5edd72bd081615e0a5219cd1737a9d79f6d
minaxolone <minaxolone@gmail.com>
2024-07-14 19:50:00 +0000

    fix build

--- web-app/build.sh
+++ web-app/build.sh
@@ -2,12 +2,10 @@
 
 set -ex
 
-npm run build
-
 CI_COMMIT_SHA="$(git rev-parse --short HEAD)"
 
 docker build \
   --build-arg CI_COMMIT_SHA="$CI_COMMIT_SHA" \
   --file ./docker/Dockerfile \
-  --tag "ghcr.io/0lnetworkcommunity/explorer/web-app:$CI_COMMIT_SHA" \
+  --tag "ghcr.io/minaxolone/explorer/web-app:$CI_COMMIT_SHA" \
   .

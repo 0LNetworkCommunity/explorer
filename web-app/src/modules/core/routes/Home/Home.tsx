341d05e338a66095029758c624c2c97ca25db10e
minaxolone <minaxolone@gmail.com>
2024-07-24 22:50:07 +0100

    cleanup home page and footer

--- web-app/src/modules/core/routes/Home/Home.tsx
+++ web-app/src/modules/core/routes/Home/Home.tsx
@@ -37,27 +37,6 @@ const Transactions: FC = () => {
 const Home: FC = () => {
   return (
     <Page>
-      <section className="pt-5 lg:pt-20 pb-12 lg:pb-8">
-        <div className="px-3">
-          <div className="flex flex-col lg:flex-row justify-between gap-6">
-            <div className="max-w-max lg:max-w-2xl">
-              <h1 className="pb-3 font-medium text-5xl text-[#141414]">0L Network Explorer</h1>
-              <p className="text-[#424242] text-base	">
-                0L is open, permissionless, and community governed. 0L combines state of the art
-                blockchain technology with truly decentralized governance.
-              </p>
-            </div>
-            <a
-              href={OL_DISCORD_URL}
-              target="_blank"
-              className="h-fit px-3.5 py-4 lg-py-3 gap-1.5 text-white bg-[#CD3B42] flex justify-center items-center font-medium rounded-md shadow-sm cursor-pointer hover:bg-red-700 transition-colors duration-150"
-            >
-              <ArrowUpRightIcon className="h-5 w-5" />
-              Join Community
-            </a>
-          </div>
-        </div>
-      </section>
       <Stats />
       <section className="mt-2 overflow-x-auto">
         <div className="my-6 px-2	flex flex-row justify-between align-center gap-3">
--- web-app/src/modules/ui/Layout/Footer/Footer.tsx
+++ web-app/src/modules/ui/Layout/Footer/Footer.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
-import Logo from '../../Logo';
 
 const CI_COMMIT_SHA: string = import.meta.env.VITE_CI_COMMIT_SHA;
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#0A0A0A] text-white">
-      <div className="pt-14 pb-8 max-w-screen-xl mx-auto px-6 md:px-24 lg:px-32 flex flex-col gap-32">
-        <div className="flex flex-col items-center justify-center gap-8 text-center">
-          <Logo withText={false} className="h-14 w-14" />
-          <p className="flex flex-col justify-center items-center gap-4">
-            <span className="text-3xl font-medium">Want to contribute to 0L Network?</span>
-            <span className="text-xl font-normal">
-              Join the most open, transparent and community driven network today.
-            </span>
-          </p>
-          <a
-            href="https://discord.com/invite/0lnetwork"
-            target="_blank"
-            rel="noopener"
-            className="px-4 py-2.5 bg-white rounded-sm text-lg text-[#525252] border border-[#E5E5E5] hover:bg-gray-300 hover:text-gray-700 transition-colors duration-150"
-          >
-            Join our discord
-          </a>
-        </div>
-        <div className="justify-center md:justify-between border-t border-t-1 flex flex-row flex-wrap pt-8 gap-3">
+      <div className="py-8 max-w-screen-xl mx-auto px-6 md:px-24 lg:px-32 flex flex-col gap-32">
+        <div className="justify-center md:justify-between flex flex-row flex-wrap gap-3">
           <span className="opacity-60 ">
-            © 2024 0L Network. All rights reserved.
-            <span className="opacity-50"> - {CI_COMMIT_SHA}</span>
+            <span className="opacity-50">{CI_COMMIT_SHA}</span>
           </span>
           <div className="flex gap-2">
             <a

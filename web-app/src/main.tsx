97101bf464f6ede041250127804cac5ccadcf019
David Boreham <david@bozemanpass.com>
2024-12-20 11:50:23 -0700

    Add back missing code

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,7 +2,10 @@
 
 import ReactDOM from 'react-dom/client';
 
+import App from './modules/core/App';
 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <App />
+);

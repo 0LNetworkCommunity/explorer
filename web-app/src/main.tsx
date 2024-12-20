a32f8dacb4f1409a8d6904d2648d3e77de186196
David Boreham <david@bozemanpass.com>
2024-12-20 11:24:35 -0700

    Remove posthog

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,39 +2,9 @@
 
 import { PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
 
 import App from './modules/core/App';
 
 import './index.css';
 
-const VITE_POSTHOG_KEY = 'phc_hPZOabiIQet1rsxRGAELYmKy8eByxgklujcj3rTz4cd';
-const VITE_POSTHOG_HOST = 'https://eu.posthog.com';
-
-const PostHogProviderWrapper = ({ children }: PropsWithChildren) => {
-  if (window.location.host === '0l.fyi') {
-    posthog.init(VITE_POSTHOG_KEY, {
-      api_host: VITE_POSTHOG_HOST,
-    });
-
-    return (
-      <PostHogProvider
-        apiKey={VITE_POSTHOG_KEY}
-        options={{ api_host: VITE_POSTHOG_HOST }}
-        client={posthog}
-      >
-        {children}
-      </PostHogProvider>
-    );
-  }
-  return <>{children}</>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <PostHogProviderWrapper>
-    <App />
-  </PostHogProviderWrapper>,
-  // </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render();

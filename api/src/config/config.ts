b6be46cd66c9106477c692c2a0bbd95efc67924d
Which Node <whichnode@whichnode.com>
2025-01-08 11:26:28 -0700

    Get RPC provider url from config

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -14,7 +14,7 @@ export default (): Config => {
     },
 
     ol: {
-      provider: 'https://rpc.0l.fyi',
+      provider: ENV.RPC_PROVIDER_URL!,
       dataApiHost: ENV.DATA_API_HOST!,
       validatorHandlesUrl: ENV.VALIDATOR_HANDLES_URL || undefined,
       communityWalletsUrl: ENV.COMMUNITY_WALLETS_URL || undefined,

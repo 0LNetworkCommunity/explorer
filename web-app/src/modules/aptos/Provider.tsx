3de761611fdc314172f4ae4c485bb4fdff70454b
hemulin <mr.hemulin@gmail.com>
2025-07-26 14:02:18 +0300

    Removed console log and fixed provider url

--- web-app/src/modules/aptos/Provider.tsx
+++ web-app/src/modules/aptos/Provider.tsx
@@ -4,7 +4,7 @@ import aptosContext from './context';
 
 const AptosProvider: FC<PropsWithChildren> = ({ children }) => {
   const client = useMemo(() => {
-    return new AptosClient('https://rpc.scan.scan.openlibra.world');
+    return new AptosClient('https://rpc.scan.openlibra.world');
   }, []);
 
   return <aptosContext.Provider value={client}>{children}</aptosContext.Provider>;
